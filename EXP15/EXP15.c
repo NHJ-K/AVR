759fa90198dc2738b8aa062575dedb341ea6edcd
MOHAMED NIHAJ K <mohamed19130391@ssmpoly.ac.in>
2022-07-03 19:25:14 +0530

    Update EXP15.c

--- EXP15/EXP15.c
+++ EXP15/EXP15.c
@@ -7,24 +7,28 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define F_CPU 1000000UL
 
 int main(void)
 {
-	DDRB |=0x20;
-	TCNT0=-32;
-	TCCR0=0x01;
-	TIMSK=(1<<TOIE0);
+	DDRB|=0x20;
+
+	TCNT0=0x0f;
+	TCCR0=0x05;
+	TIMSK|=(1<<TOIE0);
 	sei();
-	DDRC =0x00;
-	DDRD=0xFF;
-    while(1)
+	
+    DDRD=0x00;
+	DDRC=0xff;
+	while (1) 
     {
-		PORTD=~PINC;
-        //TODO:: Please write your application code 
+		
+		PORTC=~PIND;
     }
 }
-ISR(TIMER0_OVF_vect)
-{
-	TCNT0=-32;
-	PORTB ^=20;
+
+ISR(TIMER0_OVF_vect){
+	TCNT0=0x0f;
+	PORTB^=0x20;
+
 }
